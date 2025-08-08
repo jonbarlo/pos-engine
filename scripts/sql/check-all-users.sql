75a85dc9c128e80f2c1d8709748ac7edcdede278
Jonathan Barquero <jonbarlo@hotmail.com>
2025-08-07 18:07:17 -0600

    ref: Scripts clean up, Seeders cleanup

--- scripts/sql/check-all-users.sql
+++
@@ -1,4 +0,0 @@
--- Check all users and their authentication methods
-SELECT User, Host, plugin 
-FROM mysql.user 
-ORDER BY User, Host; 
--- scripts/sql/check-server-auth.sql
+++
@@ -1,3 +0,0 @@
--- Check server authentication settings
-SHOW VARIABLES LIKE 'default_authentication_plugin';
-SHOW VARIABLES LIKE 'authentication_policy'; 
